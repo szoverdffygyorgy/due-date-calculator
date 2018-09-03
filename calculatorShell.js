247619f8054156c9dffd222ef1f85ce503f361a7
szoverdffy.gyorgy <szoverdffy.gyorgy@gmail.com>
2018-09-03 14:57:11 +0200

    Script seems to be working. Tests await.

---
+++ calculatorShell.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const core = require("./calculateDueDate");
+
+const workday = {
+    start: 9,
+    end: 17,
+    hours: 8
+};
+
+module.exports = core(workday);
---
+++ spec.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const calculatorCore = require("./calculateDueDate");
+
+const dependencies = {
+    start: 9,
+    end: 17,
+    hours: 8
+};
+
+let calculator;
+
+describe("Due Date Calculator tests", () => {
+
+});
