e963fd4fb6f6efe0580d6ed67f4691b57483b5ee
Szövérdffy György <szoverdffy.gyorgy@gmail.com>
2018-09-06 23:38:08 +0200

    Requested modifications added.

--- src/calculateDueDate.js
+++ src/calculateDueDate.js
@@ -3,7 +3,7 @@
 module.exports = function createCalculator(deps) {
 	deps = deps || {};
 
-	["start", "end", "hours"].forEach(prop => {
+	["start", "end"].forEach(prop => {
 		if (!deps.hasOwnProperty(prop)) {
 			throw new Error(`${prop} is a mandatory dependency.`)
 		}
@@ -11,7 +11,7 @@ module.exports = function createCalculator(deps) {
 
 	const dayStart = deps.start;
 	const dayEnd = deps.end;
-	const numOfHours = deps.hours;
+	const numOfHours = dayEnd - dayStart;
 
 	function getDateComponents(date) {
 		return {
@@ -37,58 +37,61 @@ module.exports = function createCalculator(deps) {
 			return false;
 		}
 
-		if (date.getHours() > dayEnd) {
+		if (date.getHours() >= dayEnd) {
 			return false;
 		}
 
 		return true;
 	}
 
-	function checkOverlap(submission, turnaround) {
+	function getDayOverlap(submission, turnaround) {
+		let days = turnaround.days;
+		
 		if (submission.hour + turnaround.hours >= dayEnd && submission.minute > 0) {
-			turnaround.days++;
+			days++;
 		}
 
 		if (submission.dayNum + turnaround.days > 5) {
-			turnaround.days += 2;
+			days += 2;
 		}
 
-		turnaround.hours = (turnaround.hours + submission.hour - dayStart) % numOfHours;
+		return days;
 	}
-
+	
 	function calculateDueDate(submissionDate, turnaround) {
 		if (!(submissionDate instanceof Date) && isNaN(submissionDate)) {
 			throw new Error("Please provide a valid submission date.");
 		}
-
+		
 		if (!validateSubmissionDate(submissionDate)) {
 			throw new Error("Submission date should be on weekdays during working hours.");
 		}
-
+		
 		if (typeof turnaround !== "number" || turnaround < 0) {
 			throw new Error("turnaround should be a number.");
 		}
-
+		
 		const submissionComponents = getDateComponents(submissionDate);
 		const dueDate = submissionDate;
-
+		
 		if (turnaround + submissionComponents.hour < dayEnd) {
 			dueDate.setHours(submissionComponents.hour + turnaround);
 			return dueDate;
 		}
-
+		
 		const turnAroundTimes = {
 			days: Math.floor(turnaround / numOfHours),
 			hours: turnaround % numOfHours
 		};
-
-		checkOverlap(submissionComponents, turnAroundTimes);
-
+		
+		turnAroundTimes.days = getDayOverlap(submissionComponents, turnAroundTimes);
+		turnAroundTimes.hours = (turnAroundTimes.hours + submissionComponents.hour - dayStart) % numOfHours;
+		
 		dueDate.setDate(submissionComponents.day + turnAroundTimes.days);
 		dueDate.setHours(dayStart + turnAroundTimes.hours);
-
+		
 		return dueDate;
 	}
-
+	
 	return calculateDueDate;
 };
